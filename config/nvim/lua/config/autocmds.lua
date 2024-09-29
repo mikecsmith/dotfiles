a990f4c8e7632f3c38bbae8dbeac4a0489000574
mikecsmith <mike.c.smith85@gmail.com>
2024-09-29 10:41:20 +0100

    Migrate autocmd for ghost text to fathom

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("fathom_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "CursorMovedI" }, {
+  group = augroup("cmp_toggle_ghost_text"),
+  callback = require("fathom.cmp").toggle_ghost_text,
+})
