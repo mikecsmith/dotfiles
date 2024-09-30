5d6820a585b7a5e246d6a7984d6b37d5ce280aac
mikecsmith <mike.c.smith85@gmail.com>
2024-09-30 11:55:00 +0100

    Configure Otter

---
+++ config/nvim/after/ftplugin/quarto.lua
@@ -0,0 +1,17 @@
+local tsquery = [[
+      (fenced_code_block
+      (info_string
+        (language) @_lang
+      ) @info
+        (#match? @info "{")
+      (code_fence_content) @content (#offset! @content)
+      )
+      ((html_block) @html @combined)
+
+      ((minus_metadata) @yaml (#offset! @yaml 1 0 -1 0))
+      ((plus_metadata) @toml (#offset! @toml 1 0 -1 0))
+
+      ]]
+
+-- Activate otter for all languages, with all features using a Quarto specific treesitter query
+require("otter").activate(nil, true, true, tsquery)
---
+++ config/nvim/lua/plugins/quarto.lua
@@ -0,0 +1,37 @@
+return {
+  -- Update Marksman to work with Quarto
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {
+          filetypes = { "markdown", "markdown.mdx", "quarto" },
+          root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
+        },
+      },
+    },
+  },
+  -- Otter adds LSP capabilities to Quarto code blocks
+  {
+    "jmbuhr/otter.nvim",
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+    },
+    opts = {
+      verbose = {
+        no_code_found = false,
+      },
+    },
+  },
+  -- Extend the LazyVim markdown renderer to also handle Quarto docs
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "quarto" },
+    },
+    ft = { "quarto" },
+  },
+}
