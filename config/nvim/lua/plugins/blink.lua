f831f540676ba325f1c911df7457df3fa3a0cb7a
Michael Smith <mike.smith1@ovo.com>
2025-04-09 23:54:48 +0100

    feat: add completions to dadbod

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -9,6 +9,14 @@ return {
         enabled = true,
         sources = { "cmdline" },
       },
+      sources = {
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
     },
   },
 }
