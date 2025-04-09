8c3a9fb367c7c46bdb21390cee38497df280556b
Michael Smith <mike.smith1@ovo.com>
2025-04-09 23:51:15 +0100

    fix: Remove wezterm keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
-
-local wezterm = require("fathom.wezterm")
-
 -- stylua: ignore start
-map("n", "<M-h>", function() wezterm.move_or_fallback("h") end, { noremap = true, silent = true })
-map("n", "<M-j>", function() wezterm.move_or_fallback("j") end, { noremap = true, silent = true })
-map("n", "<M-k>", function() wezterm.move_or_fallback("k") end, { noremap = true, silent = true })
-map("n", "<M-l>", function() wezterm.move_or_fallback("l") end, { noremap = true, silent = true })
 -- stylua: ignore end
