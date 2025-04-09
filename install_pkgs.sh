b57c1eaac102ad7e44b25d0e8f3358de621f68d8
Michael Smith <mike.smith1@ovo.com>
2025-04-09 23:53:39 +0100

    chore: tidy install_pkgs.sh

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,12 +1,10 @@
-
 #!/bin/bash
 
 # Install Homebrew
-if ! command -v brew &> /dev/null; then
-    echo "Homebrew is not installed. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew is not installed. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
 
 # Install Homebrew packages
 ACCEPT_EULA=y brew bundle --file=~/.dotfiles/packages.brewfile
