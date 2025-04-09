ab89db4653595dc12cf18b83110a9265832dab57
Michael Smith <mike.smith1@ovo.com>
2025-04-09 23:58:53 +0100

    feat: add kitty support for images in nvim

--- config/nvim/lua/plugins/fathom.lua
+++ config/nvim/lua/plugins/fathom.lua
@@ -17,4 +17,51 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {},
+    },
+  },
+  {
+    "MunsMan/kitty-navigator.nvim",
+    build = {
+      "cp navigate_kitty.py ~/.config/kitty",
+      "cp pass_keys.py ~/.config/kitty",
+    },
+    keys = {
+      {
+        "<M-h>",
+        function()
+          require("kitty-navigator").navigateLeft()
+        end,
+        desc = "Move left a Split",
+        mode = { "n" },
+      },
+      {
+        "<M-j>",
+        function()
+          require("kitty-navigator").navigateDown()
+        end,
+        desc = "Move down a Split",
+        mode = { "n" },
+      },
+      {
+        "<M-k>",
+        function()
+          require("kitty-navigator").navigateUp()
+        end,
+        desc = "Move up a Split",
+        mode = { "n" },
+      },
+      {
+        "<M-l>",
+        function()
+          require("kitty-navigator").navigateRight()
+        end,
+        desc = "Move right a Split",
+        mode = { "n" },
+      },
+    },
+  },
 }
