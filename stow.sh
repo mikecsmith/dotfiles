4708df80705c86079fa1ca0fdecd705080798b72
Michael Smith <mike.smith1@ovo.com>
2025-04-16 01:33:40 +0100

    fix: add hack to make j available after refactor

--- stow.sh
+++ stow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOTFILES_DIR="$HOME/.dotfiles"  # No external overriding, directly define it
-cd $DOTFILES_DIR
+DOTFILES_DIR="$HOME/.dotfiles" # No external overriding, directly define it
+cd "$DOTFILES_DIR" || exit
 
 echo "Stowing config to $HOME/.config"
 stow --dotfiles -v -R --target="$HOME/.config" "config"
@@ -9,3 +9,7 @@ echo "Stowing home to $HOME"
 stow --dotfiles -v -R --target="$HOME" "home"
 echo "Stowing bin to $HOME/.local/bin"
 stow --dotfiles -v -R --target="$HOME/.local/bin" "bin"
+
+# TODO: Write bootstrap util for j and add to install scripts
+echo "Symlinking j to $HOME/.local/bin"
+ln -sf "$DOTFILES_DIR/apps/jira/j" "$HOME/.local/bin/j"
