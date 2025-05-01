b56c3f550bf2f75302f3a67d28e7406222c4c4d2
Michael Smith <mike.smith1@ovo.com>
2025-05-01 11:32:43 +0100

    feat: add global linting config setup

---
+++ config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,27 @@
+-- Show linters for the current buffer's file type
+vim.api.nvim_create_user_command("LintInfo", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, {})
+
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = {
+          "--config",
+          vim.fn.expand("~/.config/linters/.markdownlint-cli2.yaml"),
+          "--",
+        },
+      },
+    },
+  },
+}
