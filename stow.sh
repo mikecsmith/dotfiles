05c8f6c32466db4b4759d65d3a42985a3055895e
Michael Smith <mike.smith1@ovo.com>
2025-05-15 16:15:07 +0100

    fix: properly symlink j

--- stow.sh
+++ stow.sh
@@ -3,13 +3,12 @@
 DOTFILES_DIR="$HOME/.dotfiles" # No external overriding, directly define it
 cd "$DOTFILES_DIR" || exit
 
+ln -sf "../apps/jira/j" "bin/j"
+
 echo "Stowing config to $HOME/.config"
-stow --dotfiles -v -R --target="$HOME/.config" "config"
+stow --dotfiles -v -R --ignore="config/colima" --target="$HOME/.config" "config"
+stow -v -R --target="$HOME/.config" "config/colima/default/config.yaml"
 echo "Stowing home to $HOME"
 stow --dotfiles -v -R --target="$HOME" "home"
 echo "Stowing bin to $HOME/.local/bin"
 stow --dotfiles -v -R --target="$HOME/.local/bin" "bin"
-
-# TODO: Write bootstrap util for j and add to install scripts
-echo "Symlinking j to $HOME/.local/bin"
-ln -sf "$DOTFILES_DIR/apps/jira/j" "$HOME/.local/bin/j"
