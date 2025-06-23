2bfcb80e9ba671647c2e49e01f7efdcb03f13ffd
Michael Smith <mike.smith1@ovo.com>
2025-06-23 23:33:59 +0100

    feat: improve stow.sh

--- stow.sh
+++ stow.sh
@@ -5,10 +5,12 @@ cd "$DOTFILES_DIR" || exit
 
 ln -sf "../apps/jira/j" "bin/j"
 
-echo "Stowing config to $HOME/.config"
-stow --dotfiles -v -R --ignore="config/colima" --target="$HOME/.config" "config"
-stow -v -R --target="$HOME/.config" "config/colima/default/config.yaml"
+echo "Stowing config to $XDG_CONFIG_HOME"
+stow --dotfiles -v -R --ignore="config/colima" --target="$XDG_CONFIG_HOME" "config"
+stow -v -R --target="$XDG_CONFIG_HOME" "config/colima/default/config.yaml"
 echo "Stowing home to $HOME"
 stow --dotfiles -v -R --target="$HOME" "home"
 echo "Stowing bin to $HOME/.local/bin"
 stow --dotfiles -v -R --target="$HOME/.local/bin" "bin"
+echo "Stowing XDG_DATA to $XDG_DATA_HOME"
+stow --dotfiles -v -R --target="$XDG_DATA_HOME" "data"
