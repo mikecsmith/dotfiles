4766a536f7b2fbce934d0f43e69100cf6282a575
Michael Smith <mike.smith1@ovo.com>
2025-06-23 23:29:45 +0100

    feat: update helm lua config

--- config/nvim/lua/plugins/helm.lua
+++ config/nvim/lua/plugins/helm.lua
@@ -1,19 +1,88 @@
-LazyVim.extras.wants({
-  ft = "helm",
-  root = "Chart.yaml",
-})
-
 return {
-  { "qvalentin/helm-ls.nvim", ft = "helm" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "helm" } },
   },
   {
+    "qvalentin/helm-ls.nvim",
+    ft = "helm",
+    opts = {
+      conceal_templates = {
+        -- enable the replacement of templates with virtual text of their current values
+        enabled = true, -- this might change to false in the future
+      },
+      indent_hints = {
+        -- enable hints for indent and nindent functions
+        enabled = true,
+        -- show the hints only for the line the cursor is on
+        only_for_current_line = true,
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        helm_ls = {},
+        helm_ls = {
+          settings = {
+            ["helm-ls"] = {
+              logLevel = "info",
+              valuesFiles = {
+                mainValuesFile = "values.yaml",
+                lintOverlayValuesFile = "values.lint.yaml",
+                additionalValuesFilesGlobPattern = "values*.yaml",
+              },
+              helmLint = {
+                enabled = true,
+                ignoredMessages = {},
+              },
+              yamlls = {
+                enabled = true,
+                enabledForFilesGlob = "*.{yaml,yml}",
+                diagnosticsLimit = 50,
+                showDiagnosticsDirectly = false,
+                path = "yaml-language-server",
+                initTimeoutSeconds = 3,
+                config = {
+                  schemas = {
+                    ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.32.1-standalone-strict/all.json"] = "templates/**",
+                  },
+                  completion = true,
+                  hover = true,
+                  redhat = { telemetry = { enabled = false } },
+                  capabilities = {
+                    textDocument = {
+                      foldingRange = {
+                        dynamicRegistration = false,
+                        lineFoldingOnly = true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      setup = {
+        helm_ls = function()
+          if vim.fn.has("nvim-0.10") == 0 then
+            LazyVim.lsp.on_attach(function(client, _)
+              client.server_capabilities.documentFormattingProvider = true
+            end, "helm-ls")
+          end
+        end,
+        yamlls = function()
+          -- Prevent yamlls from attaching to Helm-related filetypes
+          LazyVim.lsp.on_attach(function(client, buffer)
+            local ft = vim.bo[buffer].filetype
+            if ft == "helm" or ft == "yaml.helm-values" then
+              vim.schedule(function()
+                vim.cmd("LspStop ++force yamlls")
+              end)
+            end
+          end, "yamlls")
+        end,
       },
     },
   },
