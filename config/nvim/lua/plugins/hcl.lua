ad50ef799aa2d75d53125e2b2ec1a3e0d5a0f9f9
Michael Smith <mike.smith1@ovo.com>
2025-06-25 10:57:08 +0100

    fix: this is what I get for copying config off the internet

--- config/nvim/lua/plugins/hcl.lua
+++ config/nvim/lua/plugins/hcl.lua
@@ -20,19 +20,4 @@ return {
       },
     },
   },
-  {
-    "nathom/filetype.nvim",
-    config = function()
-      -- Setup overrides for file extensions
-      require("filetype").setup({
-        overrides = {
-          extensions = {
-            tf = "terraform",
-            tfvars = "terraform",
-            tfstate = "json",
-          },
-        },
-      })
-    end,
-  },
 }
