276a1c6930d5b8e36b9416ecb0c6a4280ec394d7
Michael Smith <mike.smith1@ovo.com>
2026-01-29 18:39:16 +0000

    nvim install script and likec4 updates

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -56,34 +56,6 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Structurizr Autocmd - use this rather than after/ftplugin as LazyVim often overwrites
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("structurizr"),
-  pattern = "structurizr",
-  callback = function()
-    local original_cwd = vim.fn.getcwd()
-    local lsp_cwd = os.getenv("HOME") .. "/.local/share/lsp-servers/c4-dsl-language-server"
-
-    vim.cmd("cd " .. lsp_cwd)
-
-    vim.lsp.start({
-      name = "c4-language-server",
-      filetypes = { "structurizr" },
-      capabilities = vim.lsp.protocol.make_client_capabilities(),
-      flags = {
-        debounce_text_changes = 150,
-        allow_incremental_sync = true,
-        exit_timeout = 150,
-      },
-      cmd = { lsp_cwd .. "/bin/c4-language-server" },
-      root_dir = vim.fs.root(0, { "workspace.dsl" }),
-    })
-
-    vim.cmd("cd " .. original_cwd)
-    vim.cmd("setlocal nosmartindent")
-  end,
-})
-
 -- Disable prettier when DenoLS is active and no prettier config is found
 vim.api.nvim_create_autocmd("LspAttach", {
   group = augroup("deno"),
