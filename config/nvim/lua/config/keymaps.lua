0d9630dd7b9dd3937e864a4dde3b9db1ed762889
mikecsmith <mike.c.smith85@gmail.com>
2026-01-30 00:25:36 +0000

    feat: add cmd-alt-j|k for line moves in neovim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
 
--- stylua: ignore start
--- stylua: ignore end
+-- Move Lines (Normal Mode)
+map("n", "<D-A-j>", "<cmd>m .+1<cr>==回", { desc = "Move Line Down (Cmd+Alt)" })
+map("n", "<D-A-k>", "<cmd>m .-2<cr>==", { desc = "Move Line Up (Cmd+Alt)" })
+
+-- Move Lines (Visual Mode)
+map("v", "<D-A-j>", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
+map("v", "<D-A-k>", ":m '<-2<CR>gv=gv", { desc = "Move Selection Up" })
