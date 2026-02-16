f38107473f067b205df87407db00dea1baffa0d8
Michael Smith <mike.smith1@ovo.com>
2026-02-16 11:27:22 +0000

    feat: tidy up brewfiles and install_pkgs script

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -7,17 +7,12 @@ if ! command -v brew &>/dev/null; then
   test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-echo "Installing common packages..."
-brew bundle --file="$HOME/.dotfiles/base.brewfile"
-
 if [[ "$(uname -s)" == "Darwin" ]]; then
     echo "Installing macOS-specific packages..."
-    ACCEPT_EULA=y brew bundle --file="$HOME/.dotfiles/mac.brewfile"
+    ACCEPT_EULA=y brew bundle --file="$HOME/.dotfiles/Brewfile.mac"
 elif [[ "$(uname -s)" == "Linux" ]]; then
-    if [[ -f "$HOME/.dotfiles/linux.brewfile" ]]; then
-        echo "Installing Linux-specific packages..."
-        brew bundle --file="$HOME/.dotfiles/linux.brewfile"
-    fi
+    echo "Installing Linux-specific packages..."
+    ACCEPT_EULA=y brew bundle --file="$HOME/.dotfiles/Brewfile.linux"
 fi
 
 echo "All packages installed successfully."
